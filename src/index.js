f0dd1500b81a0cba1f4d619728162123e80f996e
Ariel Perez <arielperezch@gmail.com>
2017-10-01 04:27:49 -0700

    Tests pending. Basic logic is included. Integration with suggestions pending

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 var APP_ID = undefined;  // can be replaced with your app ID if publishing
-var facts = require('./facts');
-var GET_FACT_MSG_EN = [
-    "Here's your fact: "
-]
-// Test hooks - do not remove!
-exports.GetFactMsg = GET_FACT_MSG_EN;
+var suggest = require('./suggest');
+
 var APP_ID_TEST = "mochatest";  // used for mocha tests to prevent warning
 // end Test hooks
 /*
@@ -16,15 +12,36 @@ var APP_ID_TEST = "mochatest";  // used for mocha tests to prevent warning
 var languageStrings = {
     "en": {
         "translation": {
-            "FACTS": facts.FACTS_EN,
-            "SKILL_NAME": "My History Facts",  // OPTIONAL change this to a more descriptive name
-            "GET_FACT_MESSAGE": GET_FACT_MSG_EN[0],
-            "HELP_MESSAGE": "You can say tell me a fact, or, you can say exit... What can I help you with?",
+            "START": "Starting a new Draft for ",
+            "GAME_MODE": "Game Mode",
+            "PICK_SUGGESTION": "You could pick ",
+            "BAN_SUGGESTION": "You should ban ",
+            "REQUEST_GAME_MODE": "What game mode are you playing?",
+            "ASK_FIRST_PICK": "Do you have first pick?",
+            "SKILL_NAME": "DOTA Draft",  // OPTIONAL change this to a more descriptive name
+            "HELP_MESSAGE": "You can start a draft, or exit... What can I help you with?",
             "HELP_REPROMPT": "What can I help you with?",
-            "STOP_MESSAGE": "Goodbye!"
+            "STOP_MESSAGE": "Good luck, have fun!",
+            "WELCOME": "Welcome to the DOTA Draft skill. Would you like to start a draft?"
         }
     }
 };
+
+var states = {
+    IDLE: "_IDLE",      // User is not drafting
+    SETUP: "_SETUP",    // Choosing game mode
+    PICKING: "_PICKING" // User is in picking phase
+};
+
+var game_modes = {
+    CM: "captain's mode",
+    AP: "all pick"
+}
+
+var suggestion_type = {
+    PICK: "pick",
+    BAN: "ban"
+}
 
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -35,53 +52,147 @@ exports.handler = function (event, context, callback) {
     }
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(handlers, idleStateHandler, setupStateHandler, pickingStateHandler);
     alexa.execute();
 };
 
-/*
-    TODO (Part 2) add an intent for specifying a fact by year named 'GetNewYearFactIntent'
-    TODO (Part 2) provide a function for the new intent named 'GetYearFact' 
-        that emits a randomized fact that includes the year requested by the user
-        - if such a fact is not available, tell the user this and provide an alternative fact.
-    TODO (Part 3) Keep the session open by providing the fact with :askWithCard instead of :tellWithCard
-        - make sure the user knows that they need to respond
-        - provide a reprompt that lets the user know how they can respond
-    TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
-        - add message to the array GET_FACT_MSG_EN
-        - randomize this starting portion of the response for conversational variety
-*/
+const idleStateHandler = Alexa.CreateStateHandler(states.IDLE, {
+    'AMAZON.YesIntent': function () {
+        this.emitWithState("StartIntent");
+    },
+    'StartIntent': function() {
+        var intent = this.event.request.intent;
+        var mode = intent.slots.mode;
+        if (!mode.value) {
+            this.emit(":elicitSlot", "mode", this.t("REQUEST_GAME_MODE"), this.t("GAME_MODE"));
+        }
+        // Save game mode for use during picking phase
+        this.attributes.game_mode = mode.value;
+        this.attributes.player_picks = [];
+        this.attributes.player_bans = [];
+        this.attributes.enemy_picks = [];
+        this.attributes.enemy_bans = [];
+        this.attributes.rejected_picks = [];
+        this.attributes.rejected_bans = [];
+        if (mode.value == game_modes.CM) {
+            this.handler.state = states.SETUP;
+            this.emit(":ask", this.t("ASK_FIRST_PICK"), this.t("SKILL_NAME"));
+        } else {
+            this.handler.state = states.PICKING;
+            this.emit(":tell", this.t("START") + mode.value, this.t("SKILL_NAME"));
+        }
+    }
+});
+
+const setupStateHandler = Alexa.CreateStateHandler(states.SETUP, {
+    'AMAZON.YesIntent': function() {
+        this.attributes.first_pick = true;
+        this.handler.state = states.PICKING;
+        this.emitWithState("SuggestBanIntent");
+    },
+    'AMAZON.NoIntent': function() {
+        this.attributes.first_pick = false;
+        this.handler.state = states.PICKING;
+        this.emitWithState("SuggestBanIntent");
+    }
+});
+
+const pickingStateHandler = Alexa.CreateStateHandler(states.PICKING, {
+    'SuggestPickIntent': function() {
+        var suggestion = suggest.pick(
+            this.attributes.player_picks,
+            this.attributes.enemy_picks,
+            this.attributes.rejected_picks.concat(
+                this.attributes.player_bans).concat(
+                this.attributes.enemy_bans)
+        );
+
+        this.attributes.suggestion_type = suggestion_type.PICK;
+        this.attributes.last_suggestion = suggestion;
+
+        this.emit(":ask", this.t("PICK_SUGGESTION") + suggestion, this.t("SKILL_NAME"));
+    },
+    'SuggestBanIntent': function() {
+        var suggestion = suggest.ban(
+            this.attributes.player_picks,
+            this.attributes.enemy_picks,
+            this.attributes.rejected_picks.concat(
+                this.attributes.player_bans).concat(
+                this.attributes.enemy_bans)
+        );
+
+        this.attributes.suggestion_type = suggestion_type.BAN;
+        this.attributes.last_suggestion = suggestion;
+
+        this.emit(":ask", this.t("BAN_SUGGESTION") + suggestion, this.t("SKILL_NAME"));
+    },
+
+    'AMAZON.YesIntent': function() {
+        var nextIntent = "PlayerPickIntent";
+        if (this.attributes.suggestion_type == suggestion_type.PICK) {
+            this.attributes.player_picks.push(this.attributes.last_suggestion);
+        } else {
+            this.attributes.player_bans.push(this.attributes.last_suggestion);
+        }
+        this.emit(":tell", "Great!");
+    },
+    'AMAZON.NoIntent': function() {
+        var nextIntent = "SuggestPickIntent";
+        if (this.attributes.suggestion_type == suggestion_type.PICK) {
+            this.attributes.rejected_picks.push(this.attributes.last_suggestion);
+        } else {
+            nextIntent = "SuggestBanIntent";
+            this.attributes.rejected_bans.push(this.attributes.last_suggestion);
+        }
+        this.emitWithState("SuggestBanIntent");
+    },
+    'EnemyPickIntent': function() {
+        var intent = this.event.request.intent;
+        var hero = intent.slots.hero.value;
+        this.attributes.enemy_picks.push(hero);
+        this.emit(":tell", "Oh no! " + hero + " is a good pick for them.");
+    },
+    'EnemyBanIntent': function() {
+        var intent = this.event.request.intent;
+        var hero = intent.slots.hero.value;
+        this.attributes.enemy_bans.push(hero);
+        this.emit(":tell", "Curses! " + hero + " was a good pick.");
+    },
+    'PlayerPickIntent': function() {
+        var intent = this.event.request.intent;
+        var hero = intent.slots.hero.value;
+        this.attributes.player_picks.push(hero);
+        this.emit(":tell", "Good job, " + hero + " is a good pick.");
+    },
+    'PlayerBanIntent': function() {
+        var intent = this.event.request.intent;
+        var hero = intent.slots.hero.value;
+        this.attributes.player_bans.push(hero);
+        this.emit(":tell", "Well done, " + hero + " was a good pick for them.");
+    }
+});
+
 
 var handlers = {
+     // This will short-cut any incoming intent or launch requests and route them to this handler.
+    'NewSession': function() {
+        this.handler.state = states.IDLE;
+        this.emit(":ask", this.t("WELCOME"),
+            this.t("SKILL_NAME"));
+    },
     'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random fact from the facts list
-        // Use this.t() to get corresponding language data
-        var factArr = this.t('FACTS');
-        var randomFact = randomPhrase(factArr);
-
-        // Create speech output
-        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
-    },
-    'GetNewYearFactIntent': function () {
-        //TODO your code here
+        this.emit("StartIntent");
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = this.t("HELP_MESSAGE");
         var reprompt = this.t("HELP_MESSAGE");
-        this.emit(':ask', speechOutput, reprompt);
+        this.emit(":ask", speechOutput, reprompt);
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+        this.emit(":tell", this.t("STOP_MESSAGE"));
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+        this.emit(":tell", this.t("STOP_MESSAGE"));
     }
 };
 
---
+++ src/suggest.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "pick": function(picks, enemy_picks, exclude) {
+        return "Crystal Maiden";
+    },
+    "ban": function(picks, enemy_picks, exclude) {
+        return "Storm Spirit";
+    }
+}
